50d8a2065fbdf2a9498156bb5bdc65f3c35239c2
Ivo Nazlic <78ivonaz@gafe.molndal.se>
2021-03-13 14:41:54 +0100

    Add project files.

---
+++ WebbShopIvoNazlic/Database/BookDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebbShopIvoNazlic.Models;
+
+namespace WebbShopIvoNazlic.Database
+{
+    internal class BookDatabase:DbContext
+    {
+        public string DatabaseName { get; set; } = "WebbShopIvoNazlic";
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                ($@"Server = .\SQLEXPRESS;Database={Database};trusted_connection=true");
+            
+        }
+
+    }
+}
---
+++ WebbShopIvoNazlic/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebbShopIvoNazlic.Models
+{
+    internal class Book
+    {
+
+        [Key]
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+
+        public int Amount { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book: {Title}({ID}), by {Author}. Price: {Price}, Amount: {Amount}";
+        }
+    }
+}
